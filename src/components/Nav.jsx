6ecb69799c4d9a5dd96c4308717ff26f12851ec1
Adam Sobotka <vorcigernix@gmail.com>
2022-09-28 09:54:25 +0200

    navbar date

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,8 +5,8 @@ export const Nav = () => {
             <a href="/" class="inline-block ml-3"><img src="logo3.png" alt="logotype" class="rounded-full bg-black w-16 h-16 ml-5 mt-4" /></a>
 
             <input class="side-menu" type="checkbox" id="side-menu" />
-            <label class="hamb" for="side-menu"><span class="hamb-line mt-3"></span></label>
-            <nav class="nav mr-4">
+            <label class="hamb" for="side-menu"><span class="hamb-line mt-2"></span></label>
+            <nav class="nav mr-4 flex flex-col">
                 <ul class="menu">
                     <li><a href="/" class="block hover:underline">Home</a></li>
                     <li><a href="/manifesto" class="block hover:underline">Manifesto</a> </li>
@@ -16,6 +16,7 @@ export const Nav = () => {
                     <li><a href="/contributors" class="block hover:underline">Contributors</a></li>
                     <li><a href="/team" class="block hover:underline">Team</a></li>
                 </ul>
+                <span class="text-xs text-end mr-4">NOVEMBER 11-13, 2022 @ BRNO, CZECH REPUBLIC</span>
             </nav>
         </header>
     )
